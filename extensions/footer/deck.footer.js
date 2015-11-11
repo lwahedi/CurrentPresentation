59b2543bad2e524653d2720e3581e23c545390bf
lwahedi <law98@georgetown.edu>
2015-11-11 03:40:35 -0500

    First commit

---
+++ extensions/footer/deck.footer.js
@@ -0,0 +1,15 @@
+/*!
+Deck JS - deck.footer
+Copyright (c) 2011-2014 Caleb Troughton
+Dual licensed under the MIT license.
+https://github.com/imakewebthings/deck.js/blob/master/MIT-license.txt
+*/
+
+/*
+This module adds a footer to the deck.
+*/
+(function($, undefined) {
+  var $document = $(document);
+  $document.bind();
+})(jQuery, 'deck');
+
